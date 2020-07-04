80f8dbe3633fcf61c67c9cef83c66ba703686f8c
Pedro Paternostro <pedro.paternostro@gmail.com>
2020-07-04 04:14:55 -0300

    Movimento do jogador adicionado

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour, IInteractable
+{
+    public string dialogue;
+    
+    public void Interact()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb;
+
+    public float speed;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        TryMove();
+    }
+
+    public void TryMove()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        movement = movement.normalized * speed;
+
+        rb.velocity = movement;
+        animator.SetFloat("movement", movement.magnitude);
+    }
+}
